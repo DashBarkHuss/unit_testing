505d9deb431cbbca31b0b7ca924b2497afab8550
dash <dangerousdashie@gmail.com>
2019-01-07 13:59:56 -0500

    first commit

---
+++ jasmine-standalone-3.3.0/spec/AddSpec.js
@@ -0,0 +1,8 @@
+describe('add', function(){
+    it('adds two numbers', function(){
+        expect(add(4,4)).toEqual(8);
+    }); 
+    it('adds two numbers', function(){
+        expect(add(-8,4)).toEqual(-4);
+    }); 
+});
